3dbc84d6c96716f4a99551426468810d91918ebf
helloWorldRuan <ruan.rodrigues@sptech.school>
2024-06-26 11:20:05 -0300

    [feat]: create welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,19 @@
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { Header } from '../_components/header';
 
 export default function Home() {
 	return (
 		<>
 			<Header />
+
+			<div className="p-5">
+				<h2 className="text-xl font-bold">Olá, Juarez!</h2>
+
+				<p className="capitalize text-sm">
+					{format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
+				</p>
+			</div>
 		</>
 	);
 }
