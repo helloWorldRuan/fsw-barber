7f672b23ab4f1201307f13a0b2823f44a9b4aa16
helloWorldRuan <ruan.rodrigues@sptech.school>
2024-07-10 18:27:48 -0300

    [refactor] replace to semantic tags

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,9 +1,9 @@
 export function Footer() {
 	return (
-		<div className="w-full mt-40 bg-secondary py-6 px-5">
+		<footer className="w-full mt-40 bg-secondary py-6 px-5">
 			<p className="text-gray-400 text-sm font-bold opacity-85">
 				© 2024 Copyright FWS Barber
 			</p>
-		</div>
+		</footer>
 	);
 }
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,13 +5,15 @@ import { Card, CardContent } from './ui/card';
 
 export function Header() {
 	return (
-		<Card>
-			<CardContent className="py-5 flex justify-between items-center">
-				<Link href={'/'}>
-					<Image src="/logo.png" alt="logo" height={40} width={130} />
-				</Link>
-				<Menu />
-			</CardContent>
-		</Card>
+		<header>
+			<Card>
+				<CardContent className="py-5 flex justify-between items-center">
+					<Link href={'/'}>
+						<Image src="/logo.png" alt="logo" height={40} width={130} />
+					</Link>
+					<Menu />
+				</CardContent>
+			</Card>
+		</header>
 	);
 }
